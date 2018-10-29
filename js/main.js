036fe70aa196a18c6aa23cf407ad0c3befcf2709
marcus delbianco <mdelbianco9@gmail.com>
2018-10-28 22:56:50 -0700

    Created array with sums of passangers from each airline. Still working out last endpoint

--- js/main.js
+++ js/main.js
@@ -7,44 +7,130 @@ $.ajax({
 
 	dataType: 'json',
 
-	newData: [],
+	airLines: [],
 
-	finalData: [],
+	airLineNames: [],
+
+	passenger_count: [],
+
+	passengers: [],
+
+	airObject: [],
+
+	airAndPass: [],
+
+	concated: [],
+
+	/* Get needed data, create a constructor of it. Then Display data neatly in the document*/
 
 	success: function(data){
 
-		var airLines = [].sort();
+		// Air constructor
+		function Air(airLine, passengers) {
+			this.airLine = airLine;
+			this.passengers = passengers;
+			this.pushToAirObject = function(){this.airObject.push(this)};
 
+		};
+
+
+		// Gets the names of AirLines and adds it to an empty array called airLines ^
 		for(i=0; i<data.length; i++){
-			airLines.push(data[i].operating_airline);
+			this.airLines.push(data[i].operating_airline);
+			this.passenger_count.push(data[i].passenger_count);
+
+			// Concats the airline name and passanger count
+			this.airAndPass.push(data[i].operating_airline + "000"+ data[i].passenger_count);
+
 		}
 
-		for(i=0; i<airLines.length; i++){
-			if(airLines[i] !== airLines[i+1]){
-				this.newData.push(airLines[i]);
-			}else if(airLines[i+1] > airLines.length){
+		// Stuff for passenger data (Below)
+		var tempSort = this.airAndPass.sort();
+		console.log(tempSort)
+
+		var counter = 0;
+
+		for(i=0; i<tempSort.length-1; i++){
+			// If the 1st 5 characters of tempSort[i] are the same, slice the passanger count out and add them up, then add that value to the constructor function, or to an array.
+			
+			var charAt5 = tempSort[i].slice(0,8);
+			var charAt10 = tempSort[i+1].slice(0,8);
+			// var charAt0 = tempSort[i-1].slice(0,6);
+
+			if(charAt5 === charAt10){
+				var start = tempSort[i].indexOf("000")+3;
+				var end = tempSort[i].length;
+				var cut = tempSort[i].slice(start, end);
+
+				counter = counter + Number(cut);
+
+				// console.log(cut);
+			}else if(charAt5 !== charAt10){
+				// console.log("Break");
+				this.passengers.push(counter);
+				counter = 0;
+			}
+
+			
+		}
+
+		console.log(this.passengers);
+
+
+		// Stuff for passenger data (Above)
+
+
+		// Sorts the AirLine array in Alphabetical order (Multiple names for each exist)
+		var airLinesSorted = this.airLines.sort();
+
+		// Gathers the names of each airline and adds that to an empty array called airLineNames.
+		for(i=0; i<airLinesSorted.length; i++){
+			if(airLinesSorted[i] !== airLinesSorted[i+1]){
+				this.airLineNames.push(airLinesSorted[i]);
+			}else if(airLinesSorted[i+1] > airLinesSorted.length){
 				break
 			}
 		}
 
-		var nextData = this.newData.sort();
-
-		for(i=0; i<nextData.length; i++){
-			if(nextData[i] !== nextData[i+1]){
-				this.finalData.push(nextData[i]);
-			}else if(nextData[i+1] > nextData.length){
-				break
-			}
-		}
-
-
-		console.log(this.finalData)
 		
+		// Goes through Airline data and Appends list elements to Document
+		for(i=0; i<this.airLineNames.length; i++){
+			// Creates and Appends to Document
+			var createLi = document.createElement('LI');
+			createLi.textContent = this.airLineNames[i];
+			createLi.className = "list_item";
+			var list_1 = document.getElementById('list_1');
+			list_1.appendChild(createLi);
+
+			var newAir = new Air(this.airLineNames[i], "4567");
+			this.airObject.push(newAir);
+
+
+		}
+
+		// Toggles between size when a list element is clicked on
+		var liTags = document.getElementsByClassName('list_item');
+		for(i=0; i<liTags.length; i++){
+			liTags[i].onclick = function(){
+				var li = this;
+    			li.classList.toggle("expand");
+    		}
+		}
+
+
+		console.log(this.airObject);
+
+
+
+
 
 	},
 
 	type: 'GET'
 })
+	
+
+
 
 
 
